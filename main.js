d42a03497ca52d0a974039f480dc4e2f626f9a6c
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-04-22 22:38:59 -0500

    feat: :sparkles: Evento editar imagen terminado

--- main.js
+++ main.js
@@ -27,11 +27,49 @@ formulario.addEventListener("submit", (event) =>{
     btnEliminar.addEventListener("click", () => {
         divPokemon.remove()
     })
+
+    const imagen = divPokemon.querySelector("img")
+    
+    imagen.addEventListener("dblclick", () => {
+        const input = document.createElement("input")
+        input.type = "text"
+        input.value = imagen.src 
+        input.placeholder = "Nueva URL"
+        input.classList.add("brd-radius")
+        input.style.width = "100%"
+        input.style.marginTop = "10px"
+
+        const parentCard = imagen.parentElement
+
+        parentCard.replaceChild(input, imagen)
+
+        input.focus()
+
+        input.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") {
+                const nuevaImg = document.createElement("img")
+                nuevaImg.src = input.value
+                nuevaImg.alt = nombrePokemon
+                nuevaImg.className = "pokemon-image"
+                nuevaImg.onerror = () => {
+                    nuevaImg.src = 'https://placehold.co/600x400?text=Not+found'
+                }
+
+                nuevaImg.addEventListener("dblclick", () => {
+                    parentCard.replaceChild(input, nuevaImg)
+                    input.value = nuevaImg.src
+                    input.focus()
+                })
+
+                parentCard.replaceChild(nuevaImg, input)
+            }
+        })
+
+
+    })
     contenedorPokemon.appendChild(divPokemon)
     
     formulario.reset()
     
 })
-
-
 
